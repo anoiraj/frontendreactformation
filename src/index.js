9f0574b76d6dd963d09e0487a6f755772638c327
Anoir Ajej <anoirajej02@gmail.com>
2025-09-06 17:24:15 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Navbar from './components/navbar/Navbar';
+import Carousel from './components/Carousel/Carousel';
+import Pagination from './components/Pagination/Pagination';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Navbar />
+    <Carousel />
     <App />
-  </React.StrictMode>
+    <Pagination />
+
+  </React.StrictMode> 
 );
 
 // If you want to start measuring performance in your app, pass a function
